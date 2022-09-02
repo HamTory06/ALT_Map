c2158e0874932237dbb961483948d19ba626e8dc
HamTory <ghkdwndhks@dgsw.hs.kr>
2022-09-02 23:15:52 +0900

    google maps api를 따와 Manifest에 api 저장 maps xml도 함 맵 코드는 아직 안함(너무 어려움)

--- app/src/main/java/com/example/alt_map/MainActivity.kt
+++ app/src/main/java/com/example/alt_map/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.alt_map
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val fl: FrameLayout by lazy {
+        findViewById(R.id.mainFrameLayout)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val main_nav= findViewById<BottomNavigationView>(R.id.navigation)
+        main_nav.setOnClickListener(){  item ->
+            changeFragment(
+                when(){
+
+                }
+            )
+        }
+    }
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.mainFrameLayout, fragment)
+            .commit()
     }
 }
