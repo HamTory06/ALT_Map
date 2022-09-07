bd82a3c96f4187aea6f6a713d682695f25ce3ba4
HamTory <ghkdwndhks@dgsw.hs.kr>
2022-09-07 22:18:03 +0900

    드디어 구글지도 성공!! 바텀네비게이션도 됨

--- app/src/main/java/com/example/alt_map/MainActivity.kt
+++ app/src/main/java/com/example/alt_map/MainActivity.kt
@@ -2,31 +2,49 @@ package com.example.alt_map
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.FrameLayout
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private val fl: FrameLayout by lazy {
+    private val mainFrameLayout: FrameLayout by lazy {
         findViewById(R.id.mainFrameLayout)
     }
+
+    private val navigation: BottomNavigationView by lazy {
+        findViewById(R.id.navigation)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val main_nav= findViewById<BottomNavigationView>(R.id.navigation)
-        main_nav.setOnClickListener(){  item ->
-            changeFragment(
-                when(){
 
+        supportFragmentManager.beginTransaction().add(mainFrameLayout.id, Map()).commit()
+
+        navigation.setOnNavigationItemSelectedListener {
+            replaceFragment(
+                when (it.itemId) {
+                    R.id.navigation_map -> Map()
+                    R.id.navigation_weather -> weather()
+                    else -> settings()
                 }
             )
+            true
         }
+
+//        val navController = findNavController(R.id.mainFrameLayout) // 네비게이션 정의 된 걸 가져옴
+//        val main_fragment = findViewById<FrameLayout>(R.id.mainFrameLayout)
+//        val main_nav = findViewById<BottomNavigationView>(R.id.navigation)
     }
-    private fun changeFragment(fragment: Fragment) {
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.mainFrameLayout, fragment)
-            .commit()
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(mainFrameLayout.id, fragment).commit()
     }
 }
--- app/src/main/java/com/example/alt_map/weather.kt
+++ app/src/main/java/com/example/alt_map/weather.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 
 class weather : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_weather, container, false)
     }
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//    }
 }
