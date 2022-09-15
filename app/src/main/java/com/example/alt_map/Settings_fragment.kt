6c4d1d4f4cc6c83700a7de4f44aa72b723ea0ccd
HamTory <ghkdwndhks@dgsw.hs.kr>
2022-09-15 20:59:56 +0900

    아직 날씨api는 완성못함 내부 db는 완성 프로필 이름 저장

--- app/src/main/java/com/example/alt_map/Settings_fragment.kt
+++ app/src/main/java/com/example/alt_map/Settings_fragment.kt
@@ -1,10 +1,13 @@
 package com.example.alt_map
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.navigation.fragment.findNavController
 
 class Settings_fragment : Fragment() {
@@ -15,5 +18,14 @@ class Settings_fragment : Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val name = requireView().findViewById<EditText>(R.id.name)
+        val save = requireView().findViewById<Button>(R.id.save)
+        name.setText(App.prefs.myEditText)
+        if(!(App.prefs.myEditText.isNullOrBlank()))
+            Log.d("TAG", App.prefs.myEditText!!)
+
+        save.setOnClickListener{
+            App.prefs.myEditText = name.text.toString()
+        }
     }
 }
