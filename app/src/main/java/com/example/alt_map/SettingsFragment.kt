cc0e13c452a8f2da3ee28cda41b869d9e6ca526a
HamTory <ghkdwndhks@dgsw.hs.kr>
2022-09-23 14:25:31 +0900

    프로필 사진을 갤러리에서 사진을 불러와 프로필사진으로 사용할수 있도록 함(아직 덜함 ㅎ)(실험용)

--- app/src/main/java/com/example/alt_map/SettingsFragment.kt
+++ app/src/main/java/com/example/alt_map/SettingsFragment.kt
@@ -2,6 +2,7 @@ package com.example.alt_map
 
 import android.app.Activity
 import android.content.ContentResolver
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -18,11 +19,12 @@ import android.widget.Toast
 import androidx.fragment.app.findFragment
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
+import java.lang.Exception
 
 class SettingsFragment : Fragment() {
-
+    lateinit var  imageView : ImageView
     private val OPEN_GALLERY = 1
-
+    private var resolver = requireActivity().contentResolver.toString()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_settings, container, false)
@@ -31,6 +33,7 @@ class SettingsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val save = requireView().findViewById<Button>(R.id.save)
         val name = requireView().findViewById<EditText>(R.id.name)
+        val imageView = requireView().findViewById<ImageView>(R.id.circleImageView)
         name.setText(App.prefs.myEditText)
         if(!(App.prefs.myEditText.isNullOrBlank()))
             Log.d("TAG", App.prefs.myEditText!!)
@@ -39,9 +42,28 @@ class SettingsFragment : Fragment() {
             App.prefs.myEditText = name.text.toString()
         }
         val gallery = requireView().findViewById<Button>(R.id.gallery)
-        val circularImageView = requireView().findViewById<ImageView>(R.id.circleImageView)
         gallery.setOnClickListener{
             openGallery()
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?,) {
+        super.onActivityResult(requestCode, resultCode, data)
+        imageView = requireView().findViewById(R.id.circleImageView)
+        if(resultCode == Activity.RESULT_OK){ //만약에 정상적으로 코드가 실행 됬다면
+            if (requestCode == OPEN_GALLERY){ // 갤러리가 켜졌다면
+
+                var currentImageUrl : Uri? = data?.data
+
+                try { //contentResolver
+                    val bitmap = MediaStore.Images.Media.getContentUri(resolver,currentImageUrl)
+                    imageView.setImageBitmap(bitmap)
+
+                }catch (e:Exception){
+                    e.printStackTrace()
+                }
+            }
+        }else{ // 코드가 정상적으로 실행 되지 않았다면
+            Log.d("에러","something wrong")
         }
     }
     private fun openGallery(){
