934385263d5d6a6570889035e948751a0d8f63ad
황주완 <101560702+HamTory06@users.noreply.github.com>
2022-10-04 22:21:56 +0900

    commit

---
+++ app/src/main/java/com/example/alt_map/Cloud_fragment.kt
@@ -0,0 +1,20 @@
+package com.example.alt_map
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Cloud_fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_cloud, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
