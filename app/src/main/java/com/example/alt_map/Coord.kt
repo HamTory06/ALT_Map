3c2f163ef77f9c12d45fc41da87df331e335adf6
korlkm <103992732+korlkm@users.noreply.github.com>
2022-10-04 22:07:04 +0900

    commit

--- app/src/main/java/com/example/alt_map/Coord.kt
+++
@@ -1,6 +0,0 @@
-package com.example.alt_map
-
-data class Coord(
-    val lat: Double,
-    val lon: Double
-)
--- app/src/main/java/com/example/alt_map/Rain.kt
+++
@@ -1,5 +0,0 @@
-package com.example.alt_map
-
-data class Rain(
-    val `1h`: Double
-)
